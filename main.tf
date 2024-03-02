8a89157c7affc717d920daceb02cf97711c4ff6b
Adebayo Oriyomi <adebayooriyomi@gmail.com>
2024-03-02 00:01:42 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-data "google_compute_subnetwork" "app" {
-  name   = "app"
-  region = "us-east1"
+resource "google_compute_network" "app" {
+  name                    = "app"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "app" {
+  name          = "app"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.app.id
 }
 
 data "google_compute_image" "ubuntu" {
